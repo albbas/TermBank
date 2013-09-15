6858b0ecaaf14f945d4f2d89b349a5ef663d27d4
Børre Gaup <albbas@gmail.com>
2013-09-15 22:20:23 +0200

    Script to print new name spaces derived from the sd-terms

---
+++ find_namespaces.php
@@ -0,0 +1,22 @@
+<?php
+
+function print_all_namespaces($xml) {
+    $counter = 1200;
+
+    foreach ($xml->macro as $macro) {
+        print "// " . $macro['id'] . "\n";
+        foreach ($macro->label as $label) {
+            print "wfAddTermNamespace( " . $counter . ", '" . $label . "' );\n";
+            $counter++;
+            print "wfAddTermNamespace( " . $counter . ", '" . $label . "_talk' );\n";
+            $counter++;
+        }
+
+    }
+}
+
+$xml = simplexml_load_file('https://victorio.uit.no/langtech/branches/Risten_1-5-x/termdb/src/db-colls/classes/SD-class/SD-class.xml');
+
+print_all_namespaces($xml);
+
+?>
