3d5f13e6c4b539f97cd6500e06f391b1f4cd4629
Børre Gaup <albbas@gmail.com>
2013-09-16 23:36:08 +0200

    Make page name

--- sdTermImporter/Test/testSDTermImporter.php
+++ sdTermImporter/Test/testSDTermImporter.php
@@ -197,6 +197,39 @@ XML;
         $this->assertEquals($expectedResult, $result);
     }
 
+    public function testMakePageName()
+    {
+        $xmlstr = <<<XML
+<entry id="6">
+    <topicClass top="R" mid="R8100" botm="RN8120"/>
+    <entryref xml:lang="sme">
+        <entry id="m&#xE1;n&#xE1;_biilastuollu\S">
+            <common>
+                <head pos="S">m&#xE1;n&#xE1; biilastuollu</head>
+                <infl major="I" minor="g">stuollu - stuolu - stuoluide</infl>
+                <orth status="main"/>
+                <qa checked="true" when="20060106135554" who="risten"/>
+            </common>
+            <senses>
+                <sense idref="6" status="main">
+                    <topicClass botm="RN8120" mid="R8100" top="R"/>
+                    <synonyms/>
+                </sense>
+            </senses>
+        </entry>
+    </entryref>
+</entry>
+XML;
+
+        $dom = new SdTermImporter();
+        $dom->initSdClass('sd-class.xml');
+
+        $entry = new SimpleXMLElement($xmlstr);
+        $result = $dom->makePageName($entry, $entry->entryref[0]);
+        $expectedResult = "Ekologiija ja biras:máná biilastuollu";
+
+        $this->assertEquals($expectedResult, $result);
+    }
 }
 
 class SdTermImporter
@@ -243,6 +276,14 @@ class SdTermImporter
     function getQAChecked($entryref)
     {
         return $entryref->xpath('.//qa["checked"]')[0]['checked'];
+    }
+
+    function makePageName($entry, $entryref)
+    {
+        return $this->getTopicClassLangString(
+            $this->getTopicClass($entry),
+            $this->getEntryRefLang($entryref)
+            ) . ":" . $this->getHead($entryref);
     }
 }
 
