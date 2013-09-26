099cf3ed844240135275c4915e9431c811fd7f75
Børre Gaup <albbas@gmail.com>
2013-09-26 23:08:52 +0200

    Make expression pages from a two field csv file

--- expImport.php
+++ expImport.php
@@ -1,80 +1,68 @@
 <?php
 
+/*
+ * This file imports a csv file given in the expressions command line
+ * variable to termwiki
+ *
+ * The first field in the csv file is the expression
+ * The second field in the csv file is the content of the expression page
+ */
 // Standard boilerplate to define $IP
-if ( getenv( 'MW_INSTALL_PATH' ) !== false ) {
-	$IP = getenv( 'MW_INSTALL_PATH' );
+if (getenv('MW_INSTALL_PATH') !== false) {
+    $IP = getenv('MW_INSTALL_PATH');
 } else {
-	$dir = dirname( __FILE__ ); $IP = "$dir/../..";
+    $dir = dirname(__FILE__); $IP = "$dir/../..";
 }
-require_once( "$IP/maintenance/Maintenance.php" );
+require_once("$IP/maintenance/Maintenance.php");
 
 const SEPARATOR = '%';
 
 class TBImportExternalDatabase extends Maintenance {
 
-	public function __construct() {
-		parent::__construct();
-		$this->mDescription = '...';
-		$this->addOption( 'expressions', '.', true, true );
-		$this->addOption( 'source', '.', true, true );
-	}
+    public function __construct() {
+        parent::__construct();
+        $this->mDescription = '...';
+        $this->addOption('expressions', '.', true, true);
+    }
 
-	public function execute() {
-	
-		$expressions = $this->parseCSV( $this->getOption( 'expressions' ), false );
-	
-		foreach ( $expressions as $exp ) {
-			if ( !isset( $exp['nimitys'] ) ) continue;
-			$title = Title::makeTitleSafe( NS_NIMITYS, $exp['nimitys'] );
-			if ( !$title ) {
-				echo "Invalid title for {$exp['nimitys']}\n";
-				continue;
-			}
-			$exp['lähdeaineisto'] = $source;
-			$this->insert( $title, $exp );
-		}
-	}
+    public function execute() {
 
-	protected function parseCSV( $filename, $uniq = 0 ) {
-		$data = file_get_contents( $filename );
-		$rows = str_getcsv( $data, "\n" );
+        foreach($this->parseCSV($this->getOption('expressions')) as $expression => $content) {
 
-		$headerRow = array_shift( $rows );
-		$headers = str_getcsv( $headerRow, SEPARATOR );
+            $title = Title::makeTitleSafe(NS_EXPRESSION, $expression);
+            if (!$title) {
+                echo "Invalid title for {$expression}\n";
+                continue;
+            }
 
-		$output = array();
-		foreach ( $rows as $row ) {
-			$values = str_getcsv( $row, SEPARATOR );
+            $this->insert($title, $content);
+        }
 
-			if ( count( $values ) !== count( $headers ) ) {
-				echo "Apua2\n";
-				var_dump( $headers, $values ); die();
-			}
+    }
 
-			if ( is_int($uniq) ) {
-				$output[$values[$uniq]] = array_combine( $headers, $values );
-			} else {
-				$output[] = array_combine( $headers, $values );
-			}
-		}
-		return $output;
-	}
+    protected function parseCSV($filename, $uniq = 0) {
+        $data = file_get_contents($filename);
+        $rows = str_getcsv($data, "\n");
 
-	protected function insert( Title $title, $fields ) {
-	$content = "{{" . "Nimitys\n";
-		foreach ( $fields as $key => $value ) {
-			if ( $key[0] === '_' ) continue;
-			$content .= "|$key=$value\n";
-		}
-		$content .= "}}\n";
+        $output = array();
+        foreach ($rows as $row) {
+            $values = str_getcsv($row, "\t");
+            $output[$values[0]] = $values[1];
+        }
+        return $output;
+    }
 
-		$user = User::newFromName( 'Aineiston tuonti', false );
-		$page = new WikiPage( $title );
-		$page->doEdit( $content, 'Aineiston tuonti', 0, false, $user );
+    protected function insert(Title $title, $content) {
+        $content = str_replace("|language", "\n|language", $content);
+        $content = str_replace("|pos", "\n|pos", $content);
+        $content = str_replace("}}", "\n}}\n", $content);
 
-	}
+        $user = User::newFromName('SD-expression importer', false);
+        $page = new WikiPage($title);
+        $page->doEdit($content, 'SD-expression importer', 0, false, $user);
+    }
 }
 
 $maintClass = 'TBImportExternalDatabase';
-require_once( DO_MAINTENANCE );
+require_once(DO_MAINTENANCE);
 
