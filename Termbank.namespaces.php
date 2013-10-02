15db130d4d295427d6aac4cb7c61973b986b5a85
Børre Gaup <albbas@gmail.com>
2013-10-02 16:34:40 +0200

    Test to see how this works

--- Termbank.namespaces.php
+++ Termbank.namespaces.php
@@ -7,25 +7,177 @@
 
 $namespaceNames = array();
 
-// For wikis where the MyExtension extension is not installed.
-if( !defined( 'NS_MYEXTENSION' ) ) {
-        define( 'NS_MYEXTENSION', 2510 );
-}
+// // For wikis where the MyExtension extension is not installed.
+// if (!defined('NS_SERVODATDIEĐA')) {
+//     define('NS_SERVODATDIEĐA', 1202);
+// }
+//
+// if (!defined('NS_SERVODATDIEĐA_TALK')) {
+//     define('NS_SERVODATDIEĐA_TALK', 1202);
+// }
 
-if( !defined( 'NS_MYEXTENSION_TALK' ) ) {
-        define( 'NS_MYEXTENSION_TALK', 2511 );
-}
 
 /** English */
-$namespaceNames['en'] = array(
-        NS_MYEXTENSION => 'MyNamespace',
-        NS_MYEXTENSION_TALK => 'MyNamespace_talk',
+$namespaceNames['se'] = array(
+        NS_SERVODATDIEĐA => 'Servodatdieđa_se',
+        NS_SERVODATDIEĐA_TALK => 'Servodatdieđa_ságastallan_se',
 );
 
 /** Finnish (Suomi) */
-$namespaceNames['fi'] = array(
-        NS_MYEXTENSION => 'Nimiavaruuteni',
-        NS_MYEXTENSION_TALK => 'Keskustelu_nimiavaruudestani',
+$namespaceNames['nb'] = array(
+        NS_SERVODATDIEĐA => 'Samfunnsvitenskap_nb',
+        NS_SERVODATDIEĐA_TALK => 'Samfunnsvitenskap_diskusjon_nb',
 );
+
+/*
+// B
+if (!defined('NS_DÁIDDA_JA_GIRJJÁLAŠVUOHTa')) {
+    define('NS_DÁIDDA_JA_GIRJJÁLAŠVUOHTA', 1210);
+}
+
+
+
+
+// C
+if (!defined('NS_ÁSTTOÁIGI_JA_FALÁŠTALLAN')) {
+    define('NS_ÁSTTOÁIGI_JA_FALÁŠTALLAN', 1218);
+}
+
+
+
+
+// D
+if (!defined('NS_STÁDA,_ALMMOLAŠ_HÁLDDAŠEAPMI')) {
+    define('NS_STÁDA,_ALMMOLAŠ_HÁLDDAŠEAPMI', 1226);
+}
+
+
+
+
+// E
+if (!defined('NS_JURIDIHKKA')) {
+    define('NS_JURIDIHKKA', 1234);
+}
+
+
+
+// F
+if (!defined('NS_EKONOMIIJA_JA_GÁVPPAŠEAPMI')) {
+    define('NS_EKONOMIIJA_JA_GÁVPPAŠEAPMI', 1242);
+}
+
+
+
+
+// G
+if (!defined('NS_LUONDDUDIEĐA_JA_MATEMATIHKKA')) {
+    define('NS_LUONDDUDIEĐA_JA_MATEMATIHKKA', 1250);
+}
+
+
+
+
+// H
+if (!defined('NS_MEDISIIDNA')) {
+    define('NS_MEDISIIDNA', 1258);
+}
+
+
+
+
+// I
+if (!defined('NS_TEKNIHKKA,_INDUSTRIIJA,_DUODJI')) {
+    define('NS_TEKNIHKKA,_INDUSTRIIJA,_DUODJI', 1266);
+}
+
+
+
+
+// J
+if (!defined('NS_BOAZODOALLU')) {
+    define('NS_BOAZODOALLU', 1274);
+}
+
+
+
+
+// K
+if (!defined('NS_HUKSENTEKNIHKKA')) {
+    define('NS_HUKSENTEKNIHKKA', 1282);
+}
+
+
+
+
+// L
+if (!defined('NS_ÁVNNASINDUSTRIIJA')) {
+    define('NS_ÁVNNASINDUSTRIIJA', 1290);
+}
+
+//
+
+
+
+// M
+if (!defined('NS_DIHTORTEKNOLOGIIJA_JA_DIEHTOTEKNIHKKA')) {
+    define('NS_DIHTORTEKNOLOGIIJA_JA_DIEHTOTEKNIHKKA', 1298);
+}
+
+
+
+
+
+// N
+if (!defined('NS_ÁLŠATEKNIHKKA')) {
+    define('NS_ÁLŠATEKNIHKKA', 1306);
+}
+
+
+
+
+// O
+if (!defined('NS_MÁŠENTEKNIHKKA')) {
+    define('NS_MÁŠENTEKNIHKKA', 1314);
+}
+
+
+
+
+// P
+if (!defined('NS_GULAHALLANTEKNIHKKA')) {
+    define('NS_GULAHALLANTEKNIHKKA', 1322);
+}
+
+
+
+
+// Q
+if (!defined('NS_EANANDOALLU')) {
+    define('NS_EANANDOALLU', 1330);
+}
+
+
+
+
+// R
+if (!defined('NS_EKOLOGIIJA_JA_BIRAS')) {
+    define('NS_EKOLOGIIJA_JA_BIRAS', 1338);
+}
+
+
+
+
+// T
+if (!defined('NS_GUOLÁSTUS')) {
+    define('NS_GUOLÁSTUS', 1346);
+}
+
+
+
+// X
+if (!defined('NS_BÁIKENAMAT')) {
+    define('NS_BÁIKENAMAT', 1354);
+}
+*/
 
 ?>
